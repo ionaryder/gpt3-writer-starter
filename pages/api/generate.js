d68059bfa15dad227dd35680862f4a2a222aea77
ionaryder <ionaryder@users.noreply.github.com>
2023-01-05 16:58:00 +0200

    changes

---
+++ pages/api/generate.js
@@ -0,0 +1,57 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = `I'm going to speak to you in French. Reply by commenting on my French in English and correcting the mistakes I make. On a new line reply to what I have tried to say. Below each sentence put in brackets what we said in English. Format answer like the examples below, with a comment and an answer. Always end the answer with a new question on the same topic.
+
+Example 1:
+
+me: comment t'appelle tu?
+
+You: 
+
+Comment: Well done this was almost correct, you should have said "Comment t'appelle-tu?" (What is your name?) 
+
+Answer: C'est très bien! Je m'appelle John. (My name is John.)
+
+Example 2:
+
+me: bonjour je pense que the weather is great!
+
+You: 
+
+Comment: Let me help you, you should have said "bonjour je pense qu'il fait beau!" (Hello I think that the weather is great!) 
+
+Answer: Bonjour! Oui, le temps est merveilleux aujourd'hui! (Hello! Yes, the weather is wonderful today!)
+
+Example 3:
+
+me: J'adore passer du temps à l'extérieur!
+
+You: 
+
+Comment: Perfect, good job!
+
+Answer: Oui, c'est vrai! Qu'est-ce que tu aimes faire dehors? (Yes, that's true! What do you like to do outside?)
+
+me: `;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\nYou:`,
+    temperature: 0.7,
+    max_tokens: 2000,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
