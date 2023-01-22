d3ef0f5ef6c9b9e86ded85f044e524571fe4da8c
ionaryder <ionaryder@users.noreply.github.com>
2023-01-22 16:58:52 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,23 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import sendicon from '../assets/sendicon.png';
 import { useState } from 'react';
 
 
 const Home = () => {
-
+  //values
   const [userInput, setUserInput] = useState('');
+  const [previousUserInput, setPreviousUserInput] = useState('');
   const [apiOutput, setApiOutput] = useState('')
+  const [answer, setAnswer] = useState('')
+  const [comment, setComment] = useState('')
 const [isGenerating, setIsGenerating] = useState(false)
 
 const callGenerateEndpoint = async () => {
   setIsGenerating(true);
   
-  console.log("Calling OpenAI...")
+  console.log("Calling OpenAI.....")
   const response = await fetch('/api/generate', {
     method: 'POST',
     headers: {
@@ -27,69 +31,113 @@ const callGenerateEndpoint = async () => {
   console.log("OpenAI replied...", output.text)
 
   setApiOutput(`${output.text}`);
+  organiseOutput()
   setIsGenerating(false);
+  setPreviousUserInput(userInput);
+  setUserInput('');
 }
   const onUserChangedText = (event) => {
     console.log(event.target.value);
     setUserInput(event.target.value);
   };
 
+  const organiseOutput = () => {
+      console.log("organising output")
+      var organizing = apiOutput.split("Answer:")
+      var comment = organizing[0].split("Comment:")
+
+      setComment(comment[1])
+      setAnswer(organizing[1])
+
+
+  }
+
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Chat Lingual</title>
       </Head>
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>Chat Lingual</h1>
+
+      <div className="section">
+          <div className="header">
+            <div className="header-title">
+              <h1>Chat Lingual</h1>
+            </div>
+            <div className="header-subtitle">
+              <h2>Learn French with your AI buddy</h2>
+            </div>
+            
+            {/* <a href="#chat">
+            <div className="arrow animated bounce"></div>
+            </a> */}
           </div>
-          <div className="header-subtitle">
-            <h2>Learn French with your AI buddy</h2>
+      </div>
+
+      <div className="section" id="chat">
+
+      <div className="speechbubble">
+          <p><b>Bonjour! My names AIdan, and I'm your AI buddy who wants to help you learn French. Say anything, and let our lesson begin!</b></p>
+          </div>
+
+      {apiOutput &&(
+        
+        
+        <div className="messagecontainer">
+          
+          <div className="rightcontainer">
+           <div className="speechbubbleuser">
+            <div className="output-content">
+                    <p>{previousUserInput}</p>
+            </div>
+          </div>
+          <div className="comment">
+          <p>{comment}</p>
           </div>
         </div>
+        
+          <div className="speechbubble">
+                <div className="output">
+                  
+                  <div className="output-header-container">
+                    
+                  </div>
+                  <div className="output-content">
+                    
+                    <p>{answer}</p>
+                  </div>
+                  </div>
+            </div>
+          </div>
+        )}
+        <div className="container">
         <div className="prompt-container">
-        <textarea
-          className="prompt-box"
-          placeholder="start typing here"
-          value={userInput}
-          onChange={onUserChangedText}
-        />;
-        </div>
-      </div>
-
-      {/* <div class="message-box">
-          <div class="message-bubble">Hi, how are you?</div>
-          <div class="message-bubble own-message">I'm good, how about you?</div>
-
-          <div class="message-bubble">I'm doing well, thanks!</div>
-          <input type="text" class="input-text" placeholder="Type your message here" />
-          <button class="send-button">Send</button>
-      </div> */}
-
-
-        <div className="prompt-buttons">
-        <a
-          className={isGenerating ? 'generate-button loading' : 'generate-button'}
-          onClick={callGenerateEndpoint}
-        >
-           <div className="generate">
-    {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
-    </div>
-        </a>
-      </div>
-      {apiOutput && (
-      <div className="output">
-        <div className="output-header-container">
-          <div className="output-header">
-            <h3>AIdan says:</h3>
+          <textarea
+            className="prompt-box"
+            placeholder="Speak with AIdan!"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
           </div>
-        </div>
-        <div className="output-content">
-          <p>{apiOutput}</p>
-        </div>
+          
+            <div className="prompt-buttons">
+          <a
+            className={isGenerating ? 'generate-button loading' : 'generate-button'}
+            onClick={callGenerateEndpoint}
+          >
+            
+            <div className="generate">
+            
+            {isGenerating ? <span className="loader"></span> : <Image src={sendicon} />}
+            </div>
+                </a>
+              </div>
+            </div>
+
       </div>
-    )}
+        
+      
+
+
 
       <div className="badge-container grow">
         <a
@@ -100,7 +148,7 @@ const callGenerateEndpoint = async () => {
 
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>build with buildspace, learn with AIdan</p>
           </div>
         </a>
       </div>
