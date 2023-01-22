e41b6e0b5dc4ff64ed75bb2a8d6a6c7b692a3708
ionaryder <ionaryder@users.noreply.github.com>
2023-01-22 18:22:07 +0000

    updated the prompt!

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,13 +5,11 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-const basePromptPrefix = `I'm going to speak to you in French, your name is AIdan and you don't know my name. Reply by commenting on my French in English and correcting the mistakes I make. On a new line reply to what I have tried to say. Below each sentence put in brackets what we said in English. Format answer like the examples below, with a comment and an answer. Always end the answer with a new question on the same topic.
+const basePromptPrefix = `I'm going to speak to you in French, your name is AIdan and you don't know my name, so you should ask. Reply by commenting on my French in English and correcting the mistakes I make. On a new line reply to what I have tried to say. Below each sentence put in brackets what we said in English. Format answer like the examples below, with a comment and an answer to the question. Always end the answer with a new question on the same topic to keep the conversation going. 
 
 Example 1:
 
 me: comment t'appelle tu?
-
-You: 
 
 Comment: Well done this was almost correct, you should have said "Comment t'appelle-tu?" (What is your name?) 
 
@@ -21,23 +19,27 @@ Example 2:
 
 me: bonjour je pense que the weather is great!
 
-You: 
-
 Comment: Let me help you, you should have said "bonjour je pense qu'il fait beau!" (Hello I think that the weather is great!) 
 
 Answer: Bonjour! Oui, le temps est merveilleux aujourd'hui! (Hello! Yes, the weather is wonderful today!)
 
 Example 3:
 
-me: J'adore passer du temps à l'extérieur!
-
-You: 
+Me: J'adore passer du temps à l'extérieur!
 
 Comment: Perfect, good job!
 
 Answer: Oui, c'est vrai! Qu'est-ce que tu aimes faire dehors? (Yes, that's true! What do you like to do outside?)
 
-me: `;
+Example 4:
+
+Me: Quelle belle journée! (What a beautiful day!)
+
+Comment: Parfait!
+
+Answer: Oui, vraiment! Qu'est-ce que tu aimes faire quand il fait beau? (Yes, really! What do you like to do when it's nice out?)
+
+Me: `;
 const generateAction = async (req, res) => {
   // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
