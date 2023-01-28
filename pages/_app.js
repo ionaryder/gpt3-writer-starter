c57b8f54addfc260436bedc00408e2699f552d12
ionaryder <ionaryder@users.noreply.github.com>
2023-01-28 18:12:34 +0000

    changed ga

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import ReactGA from 'react-ga';
 import { options } from 'superagent';
 
 
-ReactGA.initialize('G-CM7YXRD53W', [options])
+ReactGA.initialize('G-51M5D3EE82', [options])
 
 function App({ Component, pageProps }) {
   return <Component {...pageProps} />
