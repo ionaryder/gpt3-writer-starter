90de0f09e9fbcfbe8ea6f32019b0580be5e6cf06
ionaryder <ionaryder@users.noreply.github.com>
2023-01-29 14:26:45 +0000

    pls gtag work

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,23 @@ import { options } from 'superagent';
 ReactGA.initialize('G-51M5D3EE82', [options])
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return ( 
+    <> 
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-51M5D3EE82"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-51M5D3EE82');
+        `}
+      </Script>
+      <Component {...pageProps} /> 
+    </> 
+  ) 
 }
 export default App;
