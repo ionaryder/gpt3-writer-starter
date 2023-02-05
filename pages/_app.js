55a0ab690c4d4be9c6a6e4dc4cb6569572ea3da5
ionaryder <ionaryder@users.noreply.github.com>
2023-02-05 17:35:02 +0000

    google analytics ?

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,33 @@
 import './styles.css';
 import ReactGA from 'react-ga';
 import { options } from 'superagent';
+import Script from 'next/script';
 
 
 ReactGA.initialize('G-51M5D3EE82', [options])
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} /> 
+
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-51M5D3EE82"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-51M5D3EE82');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
+  // return <Component {...pageProps} /> 
 
   
 }
